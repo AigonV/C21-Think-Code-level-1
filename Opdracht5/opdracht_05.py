2c03e6cb8c1205ff54be435256418b9ed7498a27
Mohammed Ahmed <2149869@talnet.nl>
2021-10-14 13:57:59 +0200

    mic up

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -2,9 +2,9 @@
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
 
-def favorite_games(name):
-    print ("Some of my favourite games are", name)
+def nice_games(name):
+    print("Some of my favourite games are", name)
 
-favorite_games("ROBLOX")
-favorite_games("Rocket League")
-favorite_games("Call of Duty: World at War 2")
+nice_games("ROBLOX")
+nice_games("Rocket League")
+nice_games("Call of Duty: World at War 2")
