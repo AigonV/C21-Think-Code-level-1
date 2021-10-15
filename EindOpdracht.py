1941903f87b629773df4450cf1cb7efa8c4157ae
Mohammed Ahmed <2149869@talnet.nl>
2021-10-15 14:41:02 +0200

    mic up game

---
+++ EindOpdracht.py
@@ -0,0 +1,9 @@
+name = "Mohammed Ahmed"
+print("Hello,",name,". would you like you take this interview?")
+
+response = input("mic up?")
+
+if response == "yes" or response == "Yes":
+    print("join voice call boy")
+elif response == "no" or response == "No":
+    print("ducking 🦆")
