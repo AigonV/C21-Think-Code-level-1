7e092bdfbab289d00d9ee986a93a40a68a875f89
Mohammed Ahmed <2149869@talnet.nl>
2021-11-23 16:37:06 +0100

    Opdracht

--- EindOpdracht.py
+++ EindOpdracht.py
@@ -7,3 +7,7 @@ if response == "yes" or response == "Yes":
     print("join voice call boy")
 elif response == "no" or response == "No":
     print("ducking 🦆")
+elif response == "Maybe" or response == "maybe":
+    print("wym maybe bro mic up stop ducking")
+elif not response == "no" or response == "No" or response ==  "yes" or response ==  "Yes" or response == "Maybe" or response == "maybe":
+    print("that's what I thought idiot")
